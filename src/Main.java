f1d1c90dda92a9556a06710d42c6b8a64fdb692a
Chanelle-001 <chanelleingabire01@gmail.com>
2025-04-11 21:29:48 +0200

    TaskManager

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-//Calculator cal =new Calculator();
-//
-//System.out.println("10.0+5.0="+cal.add(10.0,5.0));
-//System.out.println("10.0-5.0="+cal.subtract(10.0,5.0));
-//System.out.println("10.0*5.0="+cal.multiply(10.0,5.0));
-//System.out.println("10.0/5.0="+cal.divide(10.0,5.0));
-//System.out.println("10.0/0.0="+cal.divide(10.0,0.0));
-//System.out.println("12.0/-2.0="+cal.divide(12.0,-2.0));Calculator cal =new Calculator();
-//
+
 
             int[] grades = {85, 90, 78, 92, 88};
 
